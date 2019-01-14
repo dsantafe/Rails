0f9bf6c8ca2ed9ec17fd07f3f6d53fdb00e6d987
David Santafe Zorrilla <santafedavid01@gmail.com>
2019-01-14 12:56:14 -0500

    Add email configuration

--- rails_projects/app/config/environments/development.rb
+++ rails_projects/app/config/environments/development.rb
@@ -51,4 +51,22 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :test
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'app.com',
+    user_name:            'ingsantafedavid@gmail.com',
+    password:             'Dasanzor',
+    authentication:       'plain',
+    enable_starttls_auto: true 
+  }
 end
